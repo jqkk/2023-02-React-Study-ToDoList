f3d126619edb165f3db24ddf4c8484b86545be43
jqkk <kkj2551@naver.com>
2023-11-17 00:40:16 +0900

    setting: vite config 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import KumaUI from "@kuma-ui/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), KumaUI()],
   server: {
     port: 3000,
-  }
-})
+  },
+});
