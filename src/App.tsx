1da03e381f643de04cdbdcfdb96f56643332e3f4
jqkk <kkj2551@naver.com>
2023-11-25 21:58:33 +0900

    feat: 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import { Flex } from "@kuma-ui/core";
-
-import { Playground } from "~/components";
-
 export const App = () => {
-  return (
-    <Flex minHeight="100vh" justifyContent="center" alignItems="center">
-      <Playground />
-    </Flex>
-  );
+  return <div />;
 };
--- src/components/card/card.tsx
+++
@@ -1,5 +0,0 @@
-import { Box } from "@kuma-ui/core";
-
-export const Card = () => {
-  return <Box width="250px" height="250px" border="1px solid black"></Box>;
-};
